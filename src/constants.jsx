3f38f640c4ecaf40f0d04b4202e5c6ca2fcdf036
Vi Truong <103717482+vivitruong@users.noreply.github.com>
2023-03-06 16:28:44 +0000

    Initial commit

---
+++ src/constants.jsx
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = ' 0x0DE24F9c3962492F8c27223E4F8AE52A41aA8839';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
