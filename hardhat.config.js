0faa100face8e9e38957422ebf8725dca2797899
Vi Truong <103717482+vivitruong@users.noreply.github.com>
2023-04-26 18:37:22 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     goerli: {
       url: 'https://frosty-attentive-panorama.ethereum-goerli.discover.quiknode.pro/49cf57eed05321f3069846b84df2727fa9b6d3ad/',
-      accounts: ['52a58cc19c24c36890c0ab956e35190cf76c3ba92c230246c7556c1b01b16e57'],
+      accounts: [''],
     },
   },
 };
